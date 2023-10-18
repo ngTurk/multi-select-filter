1dfdb50fac6389259dd2b23db483d7a619d6a6fc
ngTurk <malik_alturk@hotmail.com>
2023-10-18 03:31:14 +0300

    trigger build

--- src/app/common/config.ts
+++ src/app/common/config.ts
@@ -1,5 +1,5 @@
 const isDev = process.env.NODE_ENV === "development";
 const BASE_URL = isDev ? "http://localhost:3000" : "";
-const CATEGORIES_ENDPOINT = `${process.env.HOSTNAME}/api/categories`;
+const CATEGORIES_ENDPOINT = `/api/categories`;
 
 export { CATEGORIES_ENDPOINT };
