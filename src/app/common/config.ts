23c90169ae9a0e144df67382483c2251381c96c6
ngTurk <malik_alturk@hotmail.com>
2023-10-18 04:09:18 +0300

    change base url

--- src/app/common/config.ts
+++ src/app/common/config.ts
@@ -1,5 +1,5 @@
 const isDev = process.env.NODE_ENV === "development";
 const BASE_URL = isDev ? "http://localhost:3000" : "";
-const CATEGORIES_ENDPOINT = `/api/categories`;
+const CATEGORIES_ENDPOINT = `https://multi-select-filter-zg34.vercel.app/api/categories`;
 
 export { CATEGORIES_ENDPOINT };
